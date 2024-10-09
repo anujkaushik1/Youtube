a2beae9c85d09df6d5bf7d48603943372555360f
Anuj Kaushuj <anujkaushik1512@gmail.com>
2024-10-10 00:56:21 +0530

    use ref use case

--- src/Components/Demo.js
+++ src/Components/Demo.js
@@ -1,4 +1,4 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo, useRef, useState } from "react";
 import { getPrime } from "../utils/constants";
 
 const Demo = () => {
@@ -11,14 +11,16 @@ const Demo = () => {
 
   const memoPrime = useMemo(primeFunction, [i]);
 
+  const ref = useRef(0);
+
 //   const primeNo = getPrime(i);
 
-  console.log('rendering = ', memoPrime);
+  console.log('rendering = ');
   
-
+  let count1 = 0;
   return (
-    <div className={`w-full ${to && "bg-gray-400"}`}>
-      <div className="h-96 w-1/2">
+    <div className={`w-full`}>
+      {/* <div className="h-96 w-1/2">
         <input
           type="number"
           className="w-28 border-2 border-black m-10"
@@ -31,7 +33,28 @@ const Demo = () => {
         <button className="p-2 bg-red-400" onClick={() => setTo(!to)}>
           Toggle
         </button>
-      </div>
+      </div> */}
+
+
+<button className="p-2 bg-red-400" onClick={() => setTo(!to)}>
+          Toggle
+        </button>
+
+        <h1>h</h1>
+    
+    <button className="p-2 bg-red-400" onClick={() => {count1++; console.log('counter val= ', count1);
+    }}>
+          Click 1 for let
+        </button>
+
+        <h1> hey</h1>
+
+        <button className="p-2 bg-red-400" onClick={() => {ref.current = ref.current + 10}}>
+          Click 2 for useref
+        </button>
+
+        <h1>{ref.current}</h1>
+
     </div>
   );
 };
